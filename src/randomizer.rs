98cc1a501179b3db10ea4f7b7a08c6316d787a27
kevinbogner <kevbogner@gmail.com>
2023-07-22 17:47:30 +0200

    randomize deposit_data

--- src/randomizer.rs
+++ src/randomizer.rs
@@ -1,3 +1,6 @@
+use ethereum_types::H256 as EthH256;
+use ethereum_types::H512 as EthH512;
+use hex::encode as hex_encode;
 use rand::Rng;
 use serde_json::Value;
 use std::fs::File;
@@ -21,10 +24,38 @@ pub fn randomize_deposit_data(env: &Environment) -> anyhow::Result<()> {
         let line = line?;
         let mut data: Value = serde_json::from_str(&line)?;
 
-        // Randomize deposit value
         if let Some(deposit) = data.get_mut("value") {
             let random_deposit = rng.gen_range(min_deposit..=max_deposit);
             *deposit = Value::Number(serde_json::Number::from(random_deposit));
+        }
+
+        if let Some(account) = data.get_mut("account") {
+            let random_account = format!("m/12381/3600/{}/0/0", rng.gen_range(0..=2147483647));
+            *account = Value::String(random_account);
+        }
+
+        if let Some(pubkey) = data.get_mut("pubkey") {
+            let random_pubkey: EthH512 = EthH512::random();
+            *pubkey = Value::String(format!("0x{}", hex_encode(random_pubkey.to_fixed_bytes())));
+        }
+
+        if let Some(signature) = data.get_mut("signature") {
+            let random_signature: EthH512 = EthH512::random();
+            *signature = Value::String(format!(
+                "0x{}",
+                hex_encode(random_signature.to_fixed_bytes())
+            ));
+        }
+
+        if let Some(version) = data.get_mut("version") {
+            let random_version = rng.gen_range(1..=10);
+            *version = Value::Number(serde_json::Number::from(random_version));
+        }
+
+        if let Some(withdrawal_credentials) = data.get_mut("withdrawal_credentials") {
+            let random_withdrawal_credentials: EthH256 = EthH256::random();
+            *withdrawal_credentials =
+                Value::String(hex_encode(random_withdrawal_credentials.to_fixed_bytes()));
         }
 
         let line = serde_json::to_string(&data)?;
