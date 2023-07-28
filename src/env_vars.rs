3d382e3f54ada346062a0dcacc1c8b580061b59c
kevinbogner <kevbogner@gmail.com>
2023-07-28 14:39:15 +0200

    Add unit tests for env_vars.rs and randomizer.rs

--- src/env_vars.rs
+++ src/env_vars.rs
@@ -75,3 +75,46 @@ pub fn load_environment_variables() -> Result<Environment> {
         deposit_delay_ms,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_environment_variables() {
+        env::set_var("ACC_START_INDEX", "0");
+        env::set_var("ACC_END_INDEX", "100");
+        env::set_var("DEPOSIT_AMOUNT", "32000000000");
+        env::set_var("FORK_VERSION", "0x04000000");
+        env::set_var("DEPOSIT_CONTRACT_ADDRESS", "0x...");
+        env::set_var("WITHDRAWALS_MNEMONIC", "word1 word2 word3 ... wordN");
+        env::set_var("VALIDATORS_MNEMONIC", "word1 word2 word3 ... wordN");
+        env::set_var("DEPOSIT_DATAS_FILE_LOCATION", "deposit_data.txt");
+        env::set_var("ETHEREAL_PATH", "/home/user/go/bin/ethereal");
+        env::set_var("ETH2_VAL_TOOLS_PATH", "/home/user/go/bin/eth2-val-tools");
+        env::set_var("ETH1_FROM_ADDR", "0x...");
+        env::set_var("ETH1_FROM_PRIV", "0x...");
+        env::set_var("ETH1_NETWORK", "http://127.0.0.1:8545");
+        env::set_var("DEPOSIT_DELAY_MS", "1000");
+
+        let result = load_environment_variables().unwrap();
+
+        assert_eq!(result.acc_start_index, 0);
+        assert_eq!(result.acc_end_index, 100);
+        assert_eq!(result.deposit_amount, 32000000000);
+        assert_eq!(result.fork_version, "0x04000000");
+        assert_eq!(result.deposit_contract_address, "0x...");
+        assert_eq!(result.withdrawals_mnemonic, "word1 word2 word3 ... wordN");
+        assert_eq!(result.validators_mnemonic, "word1 word2 word3 ... wordN");
+        assert_eq!(result.deposit_datas_file_location, "deposit_data.txt");
+        assert_eq!(result.ethereal_path, "/home/user/go/bin/ethereal");
+        assert_eq!(
+            result.eth2_val_tools_path,
+            "/home/user/go/bin/eth2-val-tools"
+        );
+        assert_eq!(result.eth1_from_addr, "0x...");
+        assert_eq!(result.eth1_from_priv, "0x...");
+        assert_eq!(result.eth1_network, "http://127.0.0.1:8545");
+        assert_eq!(result.deposit_delay_ms, 1000);
+    }
+}
